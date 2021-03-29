2b4325a8b8aeb61d09f2366bf197ddbe393546c7
Koji <48003543+0ji@users.noreply.github.com>
2021-03-29 10:39:35 -0400

    Completed BloodBankDAL and BloodBankLogic

--- src/main/java/dal/BloodBankDAL.java
+++ src/main/java/dal/BloodBankDAL.java
@@ -1,7 +1,10 @@
 package dal;
 
 import entity.BloodBank;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -15,12 +18,53 @@ public class BloodBankDAL extends GenericDAL<BloodBank>{
     
     @Override
     public List<BloodBank> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return findResults("BloodBank.findAll", null);
     }
 
     @Override
     public BloodBank findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Map<String, Object> map = new HashMap<>();
+        map.put( "id", id);
+        //first argument is a name given to a named query defined in appropriate entity
+        //second argument is map used for parameter substitution.
+        //parameters are names starting with : in named queries, :[name]
+        //in this case the parameter is named "id" and value for it is put in map
+        return findResult("BloodBank.findById", map);
     }
     
+    public BloodBank findByName(String name) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", name);
+        return findResult("BloodBank.findByName", map);
+    }
+    
+    public List<BloodBank> findByPrivatelyOwned(boolean privatelyOwned) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("privatelyOwned", privatelyOwned);
+        return findResults("BloodBank.findByPrivatelyOwned", map);
+    }
+    
+    public List<BloodBank> findByEstablished(Date established) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("established", established);
+        return findResults("BloodBank.findByEstablished", map);
+    }
+    
+    public List<BloodBank> findByEmployeeCount(int emplyeeCount) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("emplyeeCount", emplyeeCount);
+        return findResults("BloodBank.findByEmployeeCount", map);
+    }
+    
+    public BloodBank findByOwner(int ownerId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("ownerId", ownerId);
+        return findResult("BloodBank.findByOwner", map);
+    }
+    
+    public List<BloodBank> findContaining(String search) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("search", search);
+        return findResults("BloodBank.findContaining", map);
+    }
 }
