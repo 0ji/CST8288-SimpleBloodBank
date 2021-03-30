2c762dc16500a7f037e723a108d8b35a5eea6c6c
kam00010 <kam00010@algonquinlive.com>
2021-03-29 22:10:14 -0400

    logic Factory updated

--- src/main/java/logic/LogicFactory.java
+++ src/main/java/logic/LogicFactory.java
@@ -3,7 +3,7 @@ package logic;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
-        
+
 public abstract class LogicFactory {
 
     private static final String PACKAGE = "logic.";
@@ -12,27 +12,27 @@ public abstract class LogicFactory {
     private LogicFactory() {
     }
 
-    public static < T> T getFor( String entityName ) {
-        
-        try{
-        Class<T> type= (Class<T>)(Class.forName(PACKAGE + entityName + SUFFIX));
-        T newInstance =getFor(type);
-        return newInstance;
-        }catch(ClassNotFoundException ex){
-            ex.printStackTrace();
-        }  
-        return null;
-    }
-    
-    public static < T> T getFor( Class<T> type) {
-        try{
-        Constructor<T> declaredConstructor= type.getDeclaredConstructor();
-        T newInstance= declaredConstructor.newInstance();
-        return newInstance;
-        }catch (InstantiationException | IllegalAccessException | IllegalArgumentException |
-        InvocationTargetException | NoSuchMethodException | SecurityException ex){
+    public static < T> T getFor(String entityName) {
+
+        try {
+            Class<T> type = (Class<T>) (Class.forName(PACKAGE + entityName + SUFFIX));
+            T newInstance = getFor(type);
+            return newInstance;
+        } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
-       return null;
+        return null;
+    }
+
+    public static < T> T getFor(Class<T> type) {
+        try {
+            Constructor<T> declaredConstructor = type.getDeclaredConstructor();
+            T newInstance = declaredConstructor.newInstance();
+            return newInstance;
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException | NoSuchMethodException | SecurityException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
