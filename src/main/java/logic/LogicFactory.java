338814d08de6aac9696658d7fa9afa23f845053d
Koji <48003543+0ji@users.noreply.github.com>
2021-04-11 02:09:51 -0400

    added LogicFactory file made by Camy

--- src/main/java/logic/LogicFactory.java
+++ src/main/java/logic/LogicFactory.java
@@ -1,5 +1,13 @@
 package logic;
 
+import java.io.UncheckedIOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.juli.logging.Log;
+
 public abstract class LogicFactory {
 
     private static final String PACKAGE = "logic.";
@@ -8,9 +16,27 @@ public abstract class LogicFactory {
     private LogicFactory() {
     }
 
-    //TODO this code is not complete, it is just here for sake of programe working. need to be changed ocmpletely
-    public static < T> T getFor( String entityName ) {
-        //this casting wont be needed.
-        return (T)new AccountLogic();
+    public static < T> T getFor(String entityName) {
+        T newInstance=null;
+        try {
+            Class<T> type = (Class<T>) (Class.forName(PACKAGE + entityName + SUFFIX));
+            newInstance = getFor(type);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LogicFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return newInstance;
+    }
+
+    public static < T> T getFor(Class<T> type) {
+         T newInstance=null;
+        try {
+            Constructor<T> declaredConstructor = type.getDeclaredConstructor();
+            newInstance = declaredConstructor.newInstance();
+            
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException | NoSuchMethodException | SecurityException ex) {
+            Logger.getLogger(LogicFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return newInstance;
     }
 }
