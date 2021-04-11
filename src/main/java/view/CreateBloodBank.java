c74174f030fbd19cf325a97f73d3a0d4fab67135
Koji <48003543+0ji@users.noreply.github.com>
2021-04-11 07:03:52 -0400

    add to table (if owner is null) needs PersonDAL, PersonLogic to function with owner_id

--- src/main/java/view/CreateBloodBank.java
+++ src/main/java/view/CreateBloodBank.java
@@ -1,5 +1,7 @@
 package view;
 
+import entity.BloodBank;
+import entity.Person;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
@@ -53,12 +55,12 @@ public class CreateBloodBank extends HttpServlet {
             out.println( "<br>" );
             out.println( "Name:<br>" );
             out.printf( "<input type=\"text\" name=\"%s\" value=\"\"><br>", BloodBankLogic.NAME );
-            out.println( "<br>" );
-            out.println( "Privately Owned:<br>" );
-            out.printf( "<input type=\"text\" name=\"%s\" value=\"\"><br>", BloodBankLogic.PRIVATELY_OWNED );
+            out.println( "<br>Privately Owned:<br>" );
+            out.printf( "<input type=\"radio\" name=\"%s\" value=\"false\" checked> No ", BloodBankLogic.PRIVATELY_OWNED );
+            out.printf( "<input type=\"radio\" name=\"%s\" value=\"true\"> Yes<br>", BloodBankLogic.PRIVATELY_OWNED );
             out.println( "<br>" );
             out.println( "Established:<br>" );
-            out.printf( "<input type=\"text\" name=\"%s\" value=\"\"><br>", BloodBankLogic.ESTABLISHED );
+            out.printf( "<input type=\"text\" name=\"%s\" value=\"11-11-11 11:11:11\"><br>", BloodBankLogic.ESTABLISHED );
             out.println( "<br>" );
             out.println( "Employee Count:<br>" );
             out.printf( "<input type=\"text\" name=\"%s\" value=\"\"><br>", BloodBankLogic.EMPLOYEE_COUNT );
@@ -96,6 +98,9 @@ public class CreateBloodBank extends HttpServlet {
     /**
      * Handles the HTTP <code>GET</code> method.
      *
+     * get method is called first when requesting a URL. since this servlet will create a host this method simple
+     * delivers the html code. creation will be done in doPost method.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -111,6 +116,8 @@ public class CreateBloodBank extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
+     * this method will handle the creation of entity. as it is called by user submitting data through browser.
+     * 
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -119,10 +126,47 @@ public class CreateBloodBank extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
         log( "POST");
         BloodBankLogic bbLogic = LogicFactory.getFor("BloodBank");
-        // TODO: create the bloodbank
+//        PersonLogic pLogic = LogicFactory.getFor("Person");
+        
+        // there can be no owner for the bloodbank. so...
+        // if owner = empty, still make it (with an empty owner)
+        // otherwise make it with the person constructor
+        String owner_id = request.getParameter( BloodBankLogic.OWNER_ID );
+        log(owner_id);
+        
+        if ( owner_id.isEmpty() || owner_id.isBlank() ) { 
+            try {
+                BloodBank bloodbank = bbLogic.createEntity( request.getParameterMap() );
+                bbLogic.add( bloodbank );
+            } catch ( Exception ex ) {
+                errorMessage = ex.getMessage();
+            }
+        } 
+//        else {
+//            // if person exists
+//            Person owner = pLogic.getWithId(Integer.parseInt(owner_id));
+//            if (owner != null) {
+//                try {
+//                BloodBank bloodbank = bbLogic.createEntity( request.getParameterMap() );
+//                bloodbank.setOwner(owner);
+//                } catch (Exception ex) {
+//                    errorMessage = ex.getMessage();
+//                }
+//            //if person does not exist show error
+//            } else {
+//                errorMessage = "Owner: \"" + owner_id + "\" does not exist";
+//            }
+//        }
+        
+        if( request.getParameter( "add" ) != null ){
+            //if add button is pressed return the same page
+            processRequest( request, response );
+        } else if( request.getParameter( "view" ) != null ){
+            //if view button is pressed redirect to the appropriate table
+            response.sendRedirect( "BloodBankTable" );
+        }
     }
 
     /**
